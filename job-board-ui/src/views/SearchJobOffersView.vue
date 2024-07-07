c84331601c37171320ab9e360eb5d3ba524e73c3
Zachary Soja <zachary3003@gmail.com>
2024-07-07 18:15:28 +0200

    feat: make views look better

--- job-board-ui/src/views/SearchJobOffersView.vue
+++ job-board-ui/src/views/SearchJobOffersView.vue
@@ -1,11 +1,12 @@
 <template>
     <ViewTemplate :employee-navbar="true">
         <div class="columns">
+            <div class="column is-3">
+                <input placeholder="Location" type="text" class="input" v-model="currentSearchValue">
+                <button class="button is-info" @click="loadOffers()">Search</button>
+            </div>
             <div class="column is-8">
-                <div>
-                    <input placeholder="Location" type="text" class="input" v-model="currentSearchValue">
-                    <button class="button is-info" @click="loadOffers()">Search</button>
-                </div>
+
                 <JobOfferListItem v-for="offer in offers" :key="offer.id" :offer="offer" />
             </div>
         </div>
--- job-board-ui/src/views/ViewTemplate.vue
+++ job-board-ui/src/views/ViewTemplate.vue
@@ -1,18 +1,15 @@
 <template>
     <EmployeeNavbar v-if="employeeNavbar"/>
     <CompanyNavbar v-if="companyNavbar"/>
-    <div class="content">
+    <div>
         <slot></slot>
     </div>
 </template>
 
 <script setup lang="ts">
-import useAuthStore from '@/stores/AuthStore';
-import { AuthType } from '../stores/AuthStore';
 import EmployeeNavbar from "@/components/EmployeeNavbarComponent.vue";
 import CompanyNavbar from "@/components/CompanyNavbarComponent.vue";
 import { defineProps } from 'vue';
-const authStore = useAuthStore()
 
 const props = defineProps<{
     companyNavbar?: boolean,
